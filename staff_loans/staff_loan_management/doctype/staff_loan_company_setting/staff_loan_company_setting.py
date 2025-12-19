21825eeb786f6503e6e782f578ee9d2ede291b89
Ibrahim Malinza <ibrahim@vvsdtz.com>
2025-12-19 10:35:32 +0300

    feat: add Staff Loan Company Setting doctype and update version to 15.0.19

---
+++ staff_loans/staff_loan_management/doctype/staff_loan_company_setting/staff_loan_company_setting.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, VV System Developers LTD and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class StaffLoanCompanySetting(Document):
+	pass
---
+++ staff_loans/staff_loan_management/doctype/staff_loan_company_setting/test_staff_loan_company_setting.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, VV System Developers LTD and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestStaffLoanCompanySetting(FrappeTestCase):
+	pass
